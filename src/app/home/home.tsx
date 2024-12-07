e49df40da45b19ff9714b795f8ea4ebfdb3406f5
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-07 17:32:45 +0530

    first commit

---
+++ src/app/home/home.tsx
@@ -0,0 +1,107 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Button } from "../../components/ui/button";
+import { useRouter } from "next/navigation";
+import Post from "../post/post";
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  // Fetch posts based on input value
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          inputValue
+            ? `http://localhost:8080/posts/${inputValue}` // Search API
+            : "http://localhost:8080/posts" // Fetch all posts
+        );
+        setPosts(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, [inputValue]);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  
+  return (
+    <main className="flex flex-col items-center px-15 py-10 ">
+      
+      
+      <div className="w-full px-10 m-36">
+        <form>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              value={inputValue}
+              onChange={handleInputChange}
+              id="default-search"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline hover-outline-0"
+              placeholder="Search jobs here..."
+              required
+            />
+            <button className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <h1 className="justify-center items-center mb-20 text-neutral-100 text-2xl lg:text-4xl">
+        There are {posts?.length} job listing(s)
+      </h1>
+      <div className="flex flex-wrap gap-5 w-full items-center justify-center">
+        {posts.map((post, index) => (
+          <div
+            className=" text-sm w-3/4 lg:w-auto cursor-pointer hover:scale-105 duration-500 p-10 flex-col rounded-3xl border border-neutral-500"
+            key={post.index}
+          >
+            <h3 className="text-3xl text-neutral-100 mb-5">{post.profile}</h3>
+            <h4 className="text-neutral-300 mb-3">{post.desc}</h4>
+            <h4 className="text-neutral-300">
+              Experience : {post.exp} year(s)
+            </h4>
+            <ul className="flex text-sm flex-wrap mt-5 space-x-4 list-none">
+              {post.techs.map((tech, index) => (
+                <div className=" justify-center text-neutral-500" key={index}>
+                  <li className="">{tech}</li>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ src/app/post/post.tsx
@@ -0,0 +1,168 @@
+import { Button } from "@/components/ui/button";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import axios from "axios";
+import { log } from "console";
+import { SetStateAction, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function Post() {
+  // const [inputValue, setInputValue] = useState("");
+
+  const [formData, setFormData] = useState({
+    profile: "",
+    desc: "",
+    exp: "",
+    techs: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value, // Dynamically update the field based on the input's name
+    }));
+  };
+
+  // const handleSubmit = async (e: { preventDefault: () => void }) => {
+  //   e.preventDefault();
+
+  //   // Convert techs string into an array
+  //   const payload = {
+  //     ...formData,
+  //     techs: formData.techs.split(",").map((tech) => tech.trim()),
+  //   };
+
+  //   try {
+  //     const response = await axios.post("http://localhost:8080/post", payload, {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     console.log("Response Data:", response.data);
+  //     alert("Data submitted successfully!");
+  //   } catch (error) {
+  //     console.error("Error posting data:", error);
+  //     alert("Failed to submit data.");
+  //   }
+  // };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Convert techs string into an array
+    const payload = {
+      ...formData,
+      techs: formData.techs.split(",").map((tech) => tech.trim()),
+    };
+
+    try {
+      const response = await axios.post("http://localhost:8080/post", payload, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("Response Data:", response.data);
+      alert("Data submitted successfully!");
+    } catch (error) {
+      console.error("Error posting data:", error);
+      alert("Failed to submit data.");
+    }
+  };
+  return (
+    <main className="items-center flex flex-col w-3/4 mt-20">
+      <form onSubmit={handleSubmit} className="w-2/3 items-center">
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            value={formData.profile}
+            onChange={handleChange}
+            name="profile"
+            id="floating_email"
+            className="block py-2.5 px-0 w-full text-sm text-neutral-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-neutral-100 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-neutral-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Job Profile
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="text"
+            value={formData.desc}
+            onChange={handleChange}
+            name="desc"
+            id="floating_password"
+            className="block py-2.5 px-0 w-full text-sm text-neutral-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-neutral-100 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-neutral-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Description
+          </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
+          <input
+            type="number"
+            value={formData.exp}
+            onChange={handleChange}
+            name="exp"
+            id="floating_repeat_password"
+            className="block py-2.5 px-0 w-full text-sm text-neutral-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            htmlFor="floating_repeat_password"
+            className="peer-focus:font-medium absolute text-sm text-neutral-100 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-neutral-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Experience in years
+          </label>
+        </div>
+
+        <div className="relative flex flex-row justify-between items-center gap-2 z-0  mb-5 group">
+          <div className="flex-[7]">
+            <input
+              type="text"
+              name="techs"
+              id="floating_repeat_password"
+              className="block py-2.5 px-0 w-full text-sm text-neutral-300 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=""
+              // value={inputValue}
+              value={formData.techs}
+              onChange={handleChange}
+              required
+            />
+            <label
+              htmlFor="floating_repeat_password"
+              className="peer-focus:font-medium absolute text-sm text-neutral-100 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-neutral-500 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Technologies required (separate skills by comma)
+            </label>
+          </div>
+          {/* <Button  className="flex-[1]">Add skills</Button> */}
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
