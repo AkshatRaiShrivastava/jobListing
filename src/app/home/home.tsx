4924f7e491cf3e6fe4356bce70446a479b153f5a
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-11 22:00:23 +0530

    fixed searchbar

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -62,7 +62,7 @@ export default function Home() {
           >
             Search
           </label>
-          <div className="relative">
+          <div className="relative w-full ">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
               <svg
                 className="w-4 h-4 text-gray-500 dark:text-gray-400"
@@ -85,13 +85,13 @@ export default function Home() {
               value={inputValue}
               onChange={handleInputChange}
               id="default-search"
-              className="block w-full p-4 ps-10 text-sm text-gray-900 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline hover-outline-0"
+              className="w-auto p-4 ps-10 text-sm text-gray-900 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline hover-outline-0"
               placeholder="Search jobs here..."
               required
             />
-            <button className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            {/* <button className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               Search
-            </button>
+            </button> */}
           </div>
         </form>
       </div>
