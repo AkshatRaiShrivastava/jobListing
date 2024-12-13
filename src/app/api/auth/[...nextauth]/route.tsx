f1437f904aff07ce13659aa659e334590cbac38b
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-13 12:27:37 +0530

    added signin feature3

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
   providers: [
