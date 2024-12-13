a43a5c4899e0e4fc1ce6b98876fedb5687e82fa6
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2024-12-13 12:30:45 +0530

    linting issue

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,17 +1,18 @@
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { NextApiRequest, NextApiResponse } from "next"
 
-export default NextAuth({
+export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
+    // ...add more providers here
   ],
-  // ... other NextAuth configuration options
-});
+}
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => NextAuth(req, res, authOptions)
+
+export { handler as GET, handler as POST }
